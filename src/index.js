604d9c219025088cdb2c891e3745c662442b01d5
Keila Hadama <hadamakeila@gmail.com>
2022-01-19 15:16:11 -0300

    js recebe dados do input

--- src/index.js
+++ src/index.js
@@ -3,21 +3,27 @@ import validator from './validator.js';
 console.log(validator);
 
 function numCard(){
-    let card = document.getElementById('cardNumber').value;
-    document.getElementById('cardNumber').textContent = card;
+    let numCard = document.getElementById('cardNumber').value;
+    document.getElementById('cardNumber').textContent = numCard;
    console.log(numCard);
-    return numCard = card;
-    
-}
+   return numCard;
+}                                                                   //pegar numero
 
-// ? parseInt
+/*
+let card = document.getElementById('cardNumber').value;
+document.getElementById('cardNumber').textContent = card;
+console.log(card); */
 
-function ola(){
+function ola(){ 
     let name = document.getElementById('name').value;
-    document.getElementById("name").textContent = 'Cliente: ' + name; 
-    alert('Olá ' + name + ' confirme número de seu cartão:' + numCard);
+    document.getElementById("name").textContent = 'Cliente: ' + name;           //alert confirmar
+    let numCCard = numCard();
+    let isValidCard = validator(numCCard);
+    alert(`Olá  ${name} confirme número de seu cartão: ` + numCard());
   }
-  let confirmar = document.getElementById('submit');
-    confirmar.addEventListener('click',ola);
+  let confirmar = document.getElementById('formCartao');     //botao
+    confirmar.addEventListener('submit',ola);
+
+// document.getElementById("card").innerHTML = numCard;    manda para o p vazio o numCard cripto
 
 //manipular dom
--- src/validator.js
+++ src/validator.js
@@ -1,10 +1,25 @@
-const validator = {
-  // ...
+/*import numCard from './index.js';
+console.log(numCard); */
+
+
+
+const validator = (numCCard)=>{
+  console.log('validador');
+  console.log(numCCard);
 };
 
 export default validator;
 
-//funcao validar numero cartao
+/*funcao validar numero cartao:
 
-//funcao esconder numero cartao
+validator.isValid(creditCardNumber):
+creditCardNumber é string do numero p/ verificar
+retornar um bollean */
 
+/*funcao esconder numero cartao:
+validator.maskify(creditCardNumber)
+creditCardNumber é string do numero 
+retorna string c/ todos os num - 4 ultimos são substituidos por #
+todos os 4 ultimos mesmo quando o tamanho da string é menor
+
+*/
