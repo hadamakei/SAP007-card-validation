e28675228fa280e8846d3a9f1e23b9424a6717a1
Keila Hadama <hadamakeila@gmail.com>
2022-02-03 11:03:05 -0300

    instalado dependencia q faltava do teste;teste executado; funcoes,objeto,exportacao js mudou nome

--- src/index.js
+++ src/index.js
@@ -1,58 +1,58 @@
-import {validator, validatorMaskify} from './validator.js';                     //recebe do validador
+import validator from './validator.js';                     //recebe do validador
 
-function numCard(){
+function numCard() {
     let numCard = document.getElementById('cardNumber').value;                           //recebe input do num cartao html
-   console.log(numCard.length);
-   if ( validator(numCard) == false){                                               //conecta c/ validator
-       return false;
-   }
-   return numCard;
-}        
-
-function maskify(numcCard){                                                         // passa o numero;chama o maskify do validador; retorna valor escondido
-    return validatorMaskify(numcCard);
+    //console.log(numCard.length);
+    if (validator.isValid(numCard) == false) {                                               //conecta c/ validator
+        return false;
+    }
+    return numCard;
 }
 
-function show(){                                                                            //mostrar/ esconder div de resposta e num do input
-    let btFinal= document.getElementById("resposta");                                   
-    if (btFinal.style.display === "none"){
+function maskify(numcCard) {                                                         // passa o numero;chama o maskify do validador; retorna valor escondido
+    return validator.maskify(numcCard);
+}
+
+function show() {                                                                            //mostrar/ esconder div de resposta e num do input
+    let btFinal = document.getElementById("resposta");
+    if (btFinal.style.display === "none") {
         btFinal.style.display = "block";                                                    //mostrar div
         document.getElementById('cardNumber').value = '';                                   //esconder num
         //esconder.style.display = "none";
         //let numcCard = numcCard.this.value= " ";
-       // document.getElementsById("cardNumber").value = ''; 
-    }else{
+        // document.getElementsById("cardNumber").value = ''; 
+    } else {
         btFinal.style.display = "none";                                                     //esconder
     }
 }
 
-function output(event){ 
+function output(event) {
     let name = document.getElementById('name').value;                                       //pega nome // document.getElementById("name").textContent;   
     let numcCard = numCard();                                                               //pegar resultado do numero
     event.preventDefault();                                                             //para de recarregar page; n envia form
-    if (document.getElementById("name").value == ""){                                      //n deixa campo nome vazio
-        console.log(name);
+    if (document.getElementById("name").value == "") {                                      //n deixa campo nome vazio
+        //   console.log(name);
         document.getElementById('name').focus();                                                //foca no input do nome
         return alert('Preencha o campo Nome corretamente.');
     }
-    if (numcCard == false){                                                                 // caso numero invalido
+    if (numcCard == false) {                                                                 // caso numero invalido
         document.getElementById('cardNumber').focus();
-        return alert(`Insira um número válido.`);  
-    } 
-     else{
+        return alert(`Insira um número válido.`);
+    }
+    else {
         let number = maskify(numcCard);                                                             //caso valido resposta
         let resposta = document.getElementById('card');
-        resposta.textContent= `Olá ${name} , o número de cartão inserido foi : ${number} .`;
+        resposta.textContent = `Olá ${name} , o número de cartão inserido foi : ${number} .`;
         show();
         return false;
     }
     //alert(`Olá  ${name} confirme o número de seu cartão: ${number}.`);                     //alert confirmar
 }
 let enviar = document.getElementById('submit');                                     //botao
-enviar.addEventListener('click',output);
+enviar.addEventListener('click', output);
 
-    
-    
+
+
 
 
 // document.getElementById("card").innerHTML = numCard;    manda para o p vazio o numCard cripto
--- src/validator.js
+++ src/validator.js
@@ -9,89 +9,90 @@ function soma (pares, impares){
     return false;
   }
 }*/
+const validator = {                                                                 //vira objeto
 
-function soma (cardArray){                                                                //recebe transforme, mudei nome
-  let total = cardArray.reduce(function(primeiro,segundo){return primeiro + segundo;}, 0);
-  console.log(total);
-  if (total % 10 != 0){
-    return false;
-  }else{
-    return true;
+  soma(cardArray) {                                                                //recebe transforme, mudei nome
+    let total = cardArray.reduce(function (primeiro, segundo) { return primeiro + segundo; }, 0);
+    //console.log(total);
+    if (total % 10 != 0) {
+      return false;
+    } else {
+      return true;
+    }
+  },
+
+  isValid(numeCard) {                             //recebe numero 
+    //console.log('validador');
+    // console.log(numeCard);
+    if (numeCard.length < 11) {                                       //confere tamanho num
+      return false;
+    }
+
+    let transforme = numeCard.split('');                                  // transforma valor input em array
+    //console.log(transforme);
+    transforme.reverse();
+    // console.log(transforme);                                               // inverte ordem do array
+
+    /*let pares = [];
+    let impares = []*/
+
+    transforme = transforme.map((numero, indice) => {                       //percorre array, pega valor e o indice
+      if (indice % 2 != 0) {                                                 // verifica se indice é par
+        numero = numero * 2                                                   // dobra valor do indice par    
+        // console.log(numero);
+        if (numero >= 10) {                                                   //verifica se dobro >10
+          let duplo = String(numero).split('');                   // transforma o dobro  em string p usar split; separa os digitos cast   
+          //   console.log(numero);                                         
+          numero = parseInt(duplo[0]) + parseInt(duplo[1]);          //  digitos vira numero array e soma os digitos
+          //   console.log(numero);
+        }
+      }
+      return parseInt(numero);                                      //volta resultado p transforme
+    })
+    return this.soma(transforme);                                      //passa os num do array do transforme p/ a soma
+
+    /* versao 1
+    for(let i = 0; i < 16; i++){                         //inicia contador do array
+      transforme[i] = parseInt(transforme[i]);                //transforma em inteiro
+      console.log(i);
+      if (i % 2 != 0) {                        //verifica posicao par 
+        console.log(transforme[i]);
+        let dobro = transforme[i] * 2;                     //dobra valor do par
+        console.log(dobro);
+        if (dobro >= 10){                                   //confere se dobro é maior q 10
+          let duplo = String(dobro).split('');                   // transforma em string p usar split; separa os digitos cast   
+          console.log(duplo);                                         
+          dobro = parseInt(duplo[0]) + parseInt(duplo[1]);          //  digitos vira numero array e soma os digitos
+          console.log(dobro);
+        }
+        pares.push(dobro);                                //guarda o dobro em novo array
+        console.log(pares);
+    
+      }else {                                       //caso impar
+        impares.push(transforme[i]);                         //guarda impar em novo array
+        console.log(impares);
+      }
+    }
+    return soma(pares,impares);*/
+
+  },
+
+  maskify(numCard) {                               //recebe numero string
+    //console.log(numCard);
+    let maskify = [];                                                   //cria array
+    for (let i = 0; i < numCard.length; i++) {                           //percorre o numero de 1 em 1
+      // console.log(numCard.length)
+      if (i < numCard.length - 4) {                                       // exclui 4 ultimos
+        maskify.push("#");                                              //add icone no lugar dos numeros no array
+      } else {
+        maskify.push(numCard[i])                                      //add numeros finais no array
+      }
+    }
+    return maskify.join("");                                            //junta os num do array num string
   }
 }
 
-const validator = (numeCard) => {                             //recebe numero 
-  console.log('validador');
-  console.log(numeCard);
-  if (numeCard.length < 13){                                       //confere tamanho num
-    return false;
-  }
-  
-  let transforme = numeCard.split('');                                  // transforma valor input em array
-  console.log(transforme);
-  transforme.reverse();
-  console.log(transforme);                                               // inverte ordem do array
-
-  /*let pares = [];
-  let impares = []*/
-
-  transforme = transforme.map((numero, indice) => {                       //percorre array, pega valor e o indice
-  if (indice % 2 != 0) {                                                 // verifica se indice é par
-    numero = numero * 2                                                   // dobra valor do indice par    
-    console.log(numero);
-    if (numero >=10){                                                   //verifica se dobro >10
-      let duplo = String(numero).split('');                   // transforma o dobro  em string p usar split; separa os digitos cast   
-      console.log(numero);                                         
-      numero = parseInt(duplo[0]) + parseInt(duplo[1]);          //  digitos vira numero array e soma os digitos
-      console.log(numero);
-    } 
-  }
-  return parseInt(numero);                                      //volta resultado p transforme
-  })
-  return soma(transforme);                                      //passa os num do array do transforme p/ a soma
-
-  /* versao 1
-  for(let i = 0; i < 16; i++){                         //inicia contador do array
-    transforme[i] = parseInt(transforme[i]);                //transforma em inteiro
-    console.log(i);
-    if (i % 2 != 0) {                        //verifica posicao par 
-      console.log(transforme[i]);
-      let dobro = transforme[i] * 2;                     //dobra valor do par
-      console.log(dobro);
-      if (dobro >= 10){                                   //confere se dobro é maior q 10
-        let duplo = String(dobro).split('');                   // transforma em string p usar split; separa os digitos cast   
-        console.log(duplo);                                         
-        dobro = parseInt(duplo[0]) + parseInt(duplo[1]);          //  digitos vira numero array e soma os digitos
-        console.log(dobro);
-      }
-      pares.push(dobro);                                //guarda o dobro em novo array
-      console.log(pares);
-  
-    }else {                                       //caso impar
-      impares.push(transforme[i]);                         //guarda impar em novo array
-      console.log(impares);
-    }
-  }
-  return soma(pares,impares);*/
-  
-};
-
-const validatorMaskify = (numCard) => {                               //recebe numero string
-  console.log(numCard);
-  let maskify = [];                                                   //cria array
-  for (let i = 0; i < numCard.length; i++){                           //percorre o numero de 1 em 1
-    console.log(numCard.length)
-    if ( i < numCard.length-4){                                       // exclui 4 ultimos
-      maskify.push("*");                                              //add icone no lugar dos numeros no array
-    }else{
-        maskify.push(numCard[i])                                      //add numeros finais no array
-      }
-  }
-  return maskify.join("");                                            //junta os num do array num string
-} 
-
-export {validator,validatorMaskify};                                  //manda pro index
-
+export default validator;                     //manda pro index
 
 /*funcao validar numero cartao:
 
@@ -101,7 +102,7 @@ retornar um bollean */
 
 /*funcao esconder numero cartao:
 validator.maskify(creditCardNumber)
-creditCardNumber é string do numero 
+creditCardNumber é string do numero
 retorna string c/ todos os num - 4 ultimos são substituidos por #
 todos os 4 ultimos mesmo quando o tamanho da string é menor
 
